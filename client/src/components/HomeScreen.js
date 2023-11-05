51a6f1c73203dbe44c6df2a252219deeac5553dc
Yonger <yongerfan12@gmail.com>
2023-11-05 13:48:40 -0500

    code

---
+++ client/src/components/HomeScreen.js
@@ -0,0 +1,20 @@
+import React, { useContext, useEffect } from 'react'
+//import { GlobalStoreContext } from '../store'
+
+import AppBanner from './AppBanner';
+/*
+    This React component lists all the top5 lists in the UI.
+    
+    @author McKilla Gorilla
+*/
+const HomeScreen = () => {
+    //const { store } = useContext(GlobalStoreContext);
+    let appBanner = <AppBanner />
+
+    
+    return (
+        <div >
+        </div>)
+}
+
+export default HomeScreen;
---
+++ client/src/components/index.js
@@ -0,0 +1,21 @@
+import AppBanner from './AppBanner'
+import HomeScreen from './HomeScreen'
+import HomeWrapper from './HomeWrapper'
+import SplashScreen from './SplashScreen'
+import LoginScreen from './LoginScreen'
+import RegisterScreen from './RegisterScreen'
+
+/*
+    This serves as a module so that we can import
+    all the other components as we wish.
+    
+    @author McKilla Gorilla
+*/
+export { 
+    AppBanner,  
+    HomeScreen,
+    HomeWrapper,
+    SplashScreen,
+    LoginScreen,
+    RegisterScreen,
+    }
